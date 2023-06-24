b16a642f3a059d52fd9d86297d949e4383a90077
Tabot Joel <joeltabot56@gmail.com>
2023-06-24 11:57:00 -0700

    hello

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG={
+    DB:'mongodb+srv://joeltabot56:ePYhCglV6xohiQZB@cluster0.d2ukrob.mongodb.net/ReactFrontendProject?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
